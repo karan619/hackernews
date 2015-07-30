112fb13eb2bd2815fed58c7b655ffe3d8d1c9372
karan619 <karan.valecha92@gmail.com>
2015-07-30 22:03:45 +0530

    bug fix

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,8 +6,8 @@ class SessionsController < ApplicationController
   	user = User.find_by(email: params[:session][:email].downcase)
   	if(user && user.authenticate(params[:session][:password]))
   		session[:user_id] = user.id
-  		redirect_to login_path
       flash[:success] = "Login Successful"
+  		redirect_to root_url
   	else
       redirect_to login_path
       flash[:danger] = "Invalid email/password"
